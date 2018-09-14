195ff0f2912d26baec78c2aba0595ee17b36359d
Vlad <vladification20@gmail.com>
2018-09-14 12:38:12 +0300

    Genesis commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = process.env.PORT || 4200;
+const app = require('./app');
+
+app.listen(port, function(){
+    console.log('Server listening on port 4200')
+});
+
+
+
